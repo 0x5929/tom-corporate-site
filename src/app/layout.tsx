fdd123c719b550b25648d80a8f157bc58cfd394b
Kevin Ren <work.kevin.ren@gmail.com>
2024-02-17 14:43:14 -0800

    added scroll to top to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/features/Header'
 import Footer from '@/components/features/Footer'
+import ScrollToTop from '@/components/features/ScrollToTop'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
             <body className={`${inter.className} bg-black`}>
                 <Header />
                 {children}
+                <ScrollToTop />
                 <Footer />
             </body>
         </html>
---
+++ src/components/features/ScrollToTop.tsx
@@ -0,0 +1,54 @@
+'use client'
+import Image from 'next/image'
+import scrollToTop from '@public/scrollToTop.svg'
+
+import { useState } from 'react'
+import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
+
+const fixedScrollTopOffset: number = 30
+
+export default function ScrollToTop(): JSX.Element | null {
+    const { scrollY } = useScroll()
+    const [showBtn, setShowBtn] = useState(false)
+
+    useMotionValueEvent(scrollY, 'change', (latest: number): void => {
+        if (latest > fixedScrollTopOffset) {
+            setShowBtn(true)
+        } else {
+            setShowBtn(false)
+        }
+    })
+    const scrollTop: () => void = (): void => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+    return showBtn ? (
+        <motion.div
+            className="w-10 h-10"
+            initial={{ display: 'none' }}
+            animate={
+                showBtn
+                    ? {
+                          position: 'fixed',
+                          bottom: '23%',
+                          right: '1%',
+                          display: 'block',
+                      }
+                    : {}
+            }
+            transition={{
+                ease: 'linear',
+                duration: 0.3,
+            }}
+        >
+            <button
+                className="btn btn-square bg-transparent border-none hover:bg-transparent"
+                onClick={scrollTop}
+            >
+                <Image src={scrollToTop} color="white" alt="scroll to top" />
+            </button>
+        </motion.div>
+    ) : null
+}

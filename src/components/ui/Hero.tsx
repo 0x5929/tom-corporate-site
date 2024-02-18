4a9c603f0899ccc32c5004915382fe10006fe04d
Kevin Ren <work.kevin.ren@gmail.com>
2024-02-18 03:23:05 -0800

    fixed hero text placement

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -3,7 +3,6 @@ import styles from './hero.module.css'
 export default function Hero(): JSX.Element {
     return (
         <div className="relative sm:px-16 h-full w-full pb-16">
-            <div className={styles.imgShadow}></div>
             <div className={styles.hero}>
                 <div className="flex-col sm:pl-4 px-2 bg-transparent mt-auto h-fit inline-block align-baseline">
                     <p className="text-2xl text-white uppercase">
